794f5e3d8429ee8d5b45986ee94f56e7df1db3f7
yukihico <yukihico@gmail.com>
2016-01-17 00:06:20 +0900

    add on_import callback

--- lib/spreadsheet_model.rb
+++ lib/spreadsheet_model.rb
@@ -1,9 +1,15 @@
 require "spreadsheet_model/version"
+require 'google/api_client'
+require 'google_drive'
 
 module SpreadsheetModel
   extend ActiveSupport::Concern
 
   included do
+    def self.on_import(&block)
+      @import_callback = block
+    end
+
     def self.import
       sheet_key = ENV["GOOGLE_DRIVE_#{name.demodulize.underscore.upcase}_SHEET_KEY"]
       sheet_key = self::SHEET_KEY if defined? self::SHEET_KEY
@@ -19,7 +25,7 @@ module SpreadsheetModel
         header = rows.shift
         rows.each do |row|
           row_hash = Hash[*header.zip(row).flatten]
-          # row_hash = yield(row_hash) if block_given?
+          row_hash = @import_callback.call(row_hash) if @import_callback
           write_cache(row[0], row_hash) if row_hash.is_a? Hash
         end
       end
@@ -27,8 +33,12 @@ module SpreadsheetModel
     end
 
     def self.find(key)
-      import unless read_cache('__cached')
+      import unless cached?
       read_cache(key)
+    end
+
+    def self.cached?
+      !!read_cache('__cached')
     end
 
     private
@@ -42,7 +52,7 @@ module SpreadsheetModel
     end
 
     def self.worksheets(sheet_key)
-      client = OAuth2::Client.new(
+      client = ::OAuth2::Client.new(
         ENV['GOOGLE_DRIVE_CLIENT_ID'],
         ENV['GOOGLE_DRIVE_CLIENT_SECRET'],
         site: 'https://accounts.google.com',
@@ -53,9 +63,9 @@ module SpreadsheetModel
         refresh_token: ENV['GOOGLE_DRIVE_REFRESH_TOKEN'],
         expires_at: 3600
       }
-      auth_token = OAuth2::AccessToken.from_hash(client, auth_options)
+      auth_token = ::OAuth2::AccessToken.from_hash(client, auth_options)
       auth_token = auth_token.refresh!
-      session = GoogleDrive.login_with_oauth(auth_token.token)
+      session = ::GoogleDrive.login_with_oauth(auth_token.token)
       session.spreadsheet_by_key(sheet_key).worksheets
     end
   end
